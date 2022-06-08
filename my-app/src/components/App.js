47734b17c69fde0131a445668a6be57c04ac4f4c
Pavlo Kochuiev <pashadagilis@gmail.com>
2022-06-08 15:37:30 +0300

    HoweWork 21 is done

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -3,10 +3,28 @@ import Example from "./Example"
 
 class App extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            country: "Ukraine"
+        }
+    }
+
+    changeNameHandler = () => {
+        if (this.state.country === "Ukraine") {
+            this.setState({ country: "Ukraine is the best country" })
+        } else {
+            this.setState({ country: "Ukraine" })
+        }
+    }
+
     render() {
         return (
-            <div>
-                <Example  />
+            <div className="section">
+                <div className="title">
+                <Example message={this.state.country} />
+                <button onClick={this.changeNameHandler} className="button">Upgrade</button>
+                </div>
             </div>
         )   
     }
--- my-app/src/components/Example.js
+++ my-app/src/components/Example.js
@@ -1,28 +1,10 @@
 import React from "react";
 
 class Example extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            country: "Ukraine"
-        }
-    }
-
-    changeNameHandler = () => {
-        if (this.state.country === "Ukraine") { 
-            this.setState({country: "Ukraine is the best country"}) 
-        } else {
-            this.setState({country: "Ukraine"})
-        }
-        
-    }
 
     render() {
         return (
-            <div>
-                <p>{this.state.country}</p>
-                <button onClick={this.changeNameHandler} className="button">Upgrade</button>
-            </div>
+            <p className="text">{this.props.message}</p>
         )
     }
 }
